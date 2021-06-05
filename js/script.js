c31ca7e42a2f5f532dbb8ee434c2ad6a837f69ed
mneu windows <alberto.harres@gmail.com>
2021-06-05 16:42:36 +0200

    add loading

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
   var started = false
+  var loaded = false
+  var dots_animation = null
   const scene = $('#scene') 
   const assets = document.querySelector('a-assets')
   assets.addEventListener('loaded', (evt) => {
     console.log('loaded', evt)
+    loaded = true
     show()
+    dots_animation && clearInterval(dots_animation)
   })
   /*
   const button = $('button')
@@ -13,19 +17,42 @@ document.addEventListener("DOMContentLoaded", function(){
   })
   */
 
+  setTimeout(()=> {
+    if (loaded) return
+    $loading.removeClass("hidden")
+    dots_animation = window.setInterval( function() {
+      var dots = document.getElementById("dots");
+      if ( dots.innerHTML.length > 2 ) 
+      dots.innerHTML = "";
+      else 
+      dots.innerHTML += ".";
+    }, 500);
+  })
+
+
   const $description = $("#description")
   const $title = $("#title")
+  const $credits = $("#credits")
+  const $links = $("#links")
+  const $loading = $("#loading")
 
   const show = function () {
+    $loading.addClass('hidden');
     setTimeout(() => {
       $title.removeClass("hidden").addClass("show")
     }, 1000)
     setTimeout(() => {
       $description.removeClass("hidden").addClass("show")
+      setTimeout(()=>{
+        $credits.removeClass("hidden").addClass("show")
+      }, 100)
+      setTimeout(()=>{
+        $links.removeClass("hidden").addClass("show")
+      }, 200)
     }, 1500)
     setTimeout(() => {
       scene.addClass("show")
-    }, 2500)
+    }, 300)
   }
 
   const start = function () {
