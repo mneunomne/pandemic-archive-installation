1264b0e77c71b0aee6c4429986125a786b33e93c
mneu windows <alberto.harres@gmail.com>
2021-06-05 14:01:15 +0200

    add transitions

--- js/script.js
+++ js/script.js
@@ -1,26 +1,51 @@
 document.addEventListener("DOMContentLoaded", function(){
-  let assets = document.querySelector('a-assets')
+  var started = false
+  const scene = $('#scene') 
+  const assets = document.querySelector('a-assets')
   assets.addEventListener('loaded', (evt) => {
     console.log('loaded', evt)
-    setTimeout(() => {
-      // start() 
-    }, 2000)
+    show()
   })
-  const button = document.querySelector('button')
-  button.addEventListener('click', () => { 
+  /*
+  const button = $('button')
+  button.click(() => { 
     start()
   })
+  */
+
+  const $description = $("#description")
+  const $title = $("#title")
+
+  const show = function () {
+    setTimeout(() => {
+      $title.removeClass("hidden").addClass("show")
+    }, 1000)
+    setTimeout(() => {
+      $description.removeClass("hidden").addClass("show")
+    }, 1500)
+    setTimeout(() => {
+      scene.addClass("show")
+    }, 2500)
+  }
 
   const start = function () {
-    button.style = "display: none;"
+    if (started) return;
+    started = true;
+    // button.style = "display: none;"
     video.play();
     audios.forEach((audio) => {
-      console.log('audio',audio)
       audio.components.sound.playSound();
     })
+
   }
 
   let video = document.querySelector('video')
   let audios = document.querySelectorAll('[sound]')
+
+
+  // detect any interaction
+  document.body.addEventListener('keydown', start);
+  document.body.addEventListener('click', start);
+  document.body.addEventListener('touchstart', start);
 
 })
